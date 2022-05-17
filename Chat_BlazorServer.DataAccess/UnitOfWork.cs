695975bce334c9315cd2f4633c74c7cda16d576f
ananaslegend <myfalseakk@gmail.com>
2022-05-17 21:43:47 +0300

    Добавьте файлы проекта.

---
+++ Chat_BlazorServer.DataAccess/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat_BlazorServer.Data.Context;
+using Chat_BlazorServer.DataAccess.Abstractions;
+
+namespace Chat_BlazorServer.DataAccess
+{   //! Мені дуже сподобалась ваша реалізаця UnitOfWork, але не можу знайти щось схоже, а точно не пам'ятаю як було...
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        //public void GetRepository()
+        //{
+        //}
+    }
+}
