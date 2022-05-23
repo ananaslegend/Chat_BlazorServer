2fd9718a9af492366a07acc27b4392d84733290d
ananaslegend <myfalseakk@gmail.com>
2022-05-23 13:15:23 +0300

    Add GenericRepo

---
+++ Chat_BlazorServer.DataAccess/Abstractions/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Chat_BlazorServer.DataAccess.Abstractions
+{
+    internal interface IGenericRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predecate);
+        void Add(TEntity item);
+        void Remove(TEntity item);
+    }
+}
---
+++ Chat_BlazorServer.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Chat_BlazorServer.Data.Context;
+using Chat_BlazorServer.DataAccess.Abstractions;
+
+namespace Chat_BlazorServer.DataAccess.Repositories
+{
+    internal class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity: class
+    {
+        protected readonly ApplicationContext _context;
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public void Add(TEntity item)
+        {
+            _context.Set<TEntity>().Add(item);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predecate)
+        {
+            return _context.Set<TEntity>().Where(predecate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity item)
+        {
+            _context.Set<TEntity>().Remove(item);
+        }
+    }
+}
